28664acd54366bbdf1960e8dd76aa1f13f39aefa
paris <paris@macbookpro.mynetworksettings.com>
2025-06-07 11:01:27 -0400

    wait no use t3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,69 @@
-'use client';
+import Link from "next/link";
 
-import { useChat } from '@ai-sdk/react';
-import { useRef, useState } from 'react';
+import { LatestPost } from "~/app/_components/post";
+import { auth } from "~/server/auth";
+import { api, HydrateClient } from "~/trpc/server";
 
-export default function Page() {
-  const { messages, input, handleSubmit, handleInputChange, status } =
-    useChat();
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await auth();
 
-  const [files, setFiles] = useState<FileList | undefined>(undefined);
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  if (session?.user) {
+    void api.post.getLatest.prefetch();
+  }
 
   return (
-    <div>
-      <div>
-        {messages.map(message => (
-          <div key={message.id}>
-            <div>{`${message.role}: `}</div>
-
-            <div>
-              {message.content}
-
-              <div>
-                {message.experimental_attachments
-                  ?.filter(attachment =>
-                    attachment.contentType.startsWith('image/'),
-                  )
-                  .map((attachment, index) => (
-                    <img
-                      key={`${message.id}-${index}`}
-                      src={attachment.url}
-                      alt={attachment.name}
-                    />
-                  ))}
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          </h1>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+              href="https://create.t3.gg/en/usage/first-steps"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <div className="text-lg">
+                Just the basics - Everything you need to know to set up your
+                database and authentication.
               </div>
+            </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+              href="https://create.t3.gg/en/introduction"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Documentation →</h3>
+              <div className="text-lg">
+                Learn more about Create T3 App, the libraries it uses, and how
+                to deploy it.
+              </div>
+            </Link>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-2xl text-white">
+              {hello ? hello.greeting : "Loading tRPC query..."}
+            </p>
+
+            <div className="flex flex-col items-center justify-center gap-4">
+              <p className="text-center text-2xl text-white">
+                {session && <span>Logged in as {session.user?.name}</span>}
+              </p>
+              <Link
+                href={session ? "/api/auth/signout" : "/api/auth/signin"}
+                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+              >
+                {session ? "Sign out" : "Sign in"}
+              </Link>
             </div>
           </div>
-        ))}
-      </div>
 
-      <form
-        onSubmit={event => {
-          handleSubmit(event, {
-            experimental_attachments: files,
-          });
-
-          setFiles(undefined);
-
-          if (fileInputRef.current) {
-            fileInputRef.current.value = '';
-          }
-        }}
-      >
-        <input
-          type="file"
-          onChange={event => {
-            if (event.target.files) {
-              setFiles(event.target.files);
-            }
-          }}
-          multiple
-          ref={fileInputRef}
-        />
-        <input
-          value={input}
-          placeholder="Send message..."
-          onChange={handleInputChange}
-          disabled={status !== 'ready'}
-        />
-      </form>
-    </div>
+          {session?.user && <LatestPost />}
+        </div>
+      </main>
+    </HydrateClient>
   );
 }
